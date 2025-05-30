aa2f7f44ab280b9cf81341eeb98fd3c31997e678
ageng <ageng@monstera.id>
2025-05-30 17:38:42 +0700

    Update yaml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+          <li className="tracking-[-.01em]"> asdads
+            Save and see your changes instantly. asdsaasdadasdasd
           </li>
         </ol>
 
